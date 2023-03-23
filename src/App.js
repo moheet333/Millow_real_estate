73028eb93ee389268b42ccf7a4382235c3a06a61
moheet333 <moheetshendarkar@gmail.com>
2023-03-23 23:35:51 +0530

    ***important*** Navbar done.

--- src/App.js
+++ src/App.js
@@ -12,13 +12,55 @@ import Escrow from "./abis/Escrow.json";
 
 // Config
 import config from "./config.json";
+const getEthereumObject = () => window.ethereum;
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const findMetaMaskAccount = async () => {
+    try {
+      const ethereum = getEthereumObject();
+      if (!ethereum) {
+        console.log("Make sure you have Metamask installed");
+        return null;
+      }
+      console.log("We have ethereum object: ", ethereum);
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      ethereum.on("accountsChanged", async () => {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const account = ethers.utils.getAddress(accounts[0]);
+        setAccount(account);
+      });
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Authorized account found! ", account);
+        return account;
+      } else {
+        console.log("No authorized account found!");
+        return null;
+      }
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    async function loadData() {
+      const account = await findMetaMaskAccount();
+      if (account !== null) {
+        setAccount(account);
+      }
+    }
+
+    loadData();
+  }, []);
+
   return (
     <div>
-      <div className="cards__section">
-        <h3>Welcome to Millow</h3>
-      </div>
+      <Navigation account={account} setAccount={setAccount} />
+      <div className="cards__section"></div>
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,58 @@
-import logo from '../assets/logo.svg';
+import { ethers } from "ethers";
+import logo from "../assets/logo.svg";
+const getEthereumObject = () => window.ethereum;
 
 const Navigation = ({ account, setAccount }) => {
+  const connectWallet = async () => {
+    try {
+      const ethereum = await getEthereumObject();
 
-}
+      if (!ethereum) {
+        alert("Get Metamask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected!", accounts[0]);
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <nav>
+      <ul className="nav__links">
+        <li>
+          <a href="#">Buy</a>
+        </li>
+        <li>
+          <a href="#">Rent</a>
+        </li>
+        <li>
+          <a href="#">Sell</a>
+        </li>
+      </ul>
+
+      <div className="nav__brand">
+        <img src={logo} alt="Logo" />
+        <h1>Millow</h1>
+      </div>
+
+      {account ? (
+        <button type="button" className="nav__connect">
+          {account.slice(0, 6) + "..." + account.slice(38, 42)}
+        </button>
+      ) : (
+        <button type="button" className="nav__connect" onClick={connectWallet}>
+          Connect
+        </button>
+      )}
+    </nav>
+  );
+};
 
 export default Navigation;
